dcee402111a99fc50c1df479fab9d927c35d0a66
Antoine James Tournepiche <antoinejt.serveur@gmail.com>
2020-11-07 15:42:16 +0100

    Add IsMultipleOf function, width and height are Screen ctor params, etc.

--- src/Screen.cpp
+++ src/Screen.cpp
@@ -1,21 +1,35 @@
 #include "Screen.hpp"
 
 #include <cassert>
+#include <cmath>
 
 #include "sdl.hpp"
 #include "sdl_assert.hpp"
 #include "sdl_renderer.hpp"
 #include "sdl_window.hpp"
 
-Chip8::Screen::Screen(SDL2::SDL& sdl)
+bool IsMultipleOf(const int base, const int number)
+{
+    double d = number;
+    while (d > base)
+    {
+        d /= base;
+    }
+    return std::round(d) == d;
+}
+
+Chip8::Screen::Screen(SDL2::SDL& sdl, int width, int height)
 {
     assert(sdl.Running());
+    assert(IsMultipleOf(Chip8::base_width, width));
+    assert(IsMultipleOf(Chip8::base_height, height));
+    assert(width / height == base_width / base_height);
 
     // TODO Faire ça proprement, pour l'instant il s'agit de tests
     SDL2::Window win = SDL2::Window("Chip8Emu",
         SDL_WINDOWPOS_CENTERED,
         SDL_WINDOWPOS_CENTERED,
-        1000, 1000, 0);
+        width, height, 0);
 
     SDL2::Renderer renderer = SDL2::Renderer(win.Data(), -1, SDL_RENDERER_ACCELERATED);
 
@@ -25,8 +39,6 @@ Chip8::Screen::Screen(SDL2::SDL& sdl)
     sdl_assert(renderer.RenderClear());
     // renders screen
     renderer.RenderPresent();
-
-    SDL_Delay(10);
 
     sdl_assert(renderer.SetRenderDrawColor(255, 255, 255, 255));
     {
--- src/Screen.hpp
+++ src/Screen.hpp
@@ -4,9 +4,13 @@
 
 namespace Chip8
 {
+    // Chip8 Base screen size
+    constexpr int base_width = 64;
+    constexpr int base_height = 32;
+
     class Screen
     {
     public:
-        explicit Screen(SDL2::SDL& sdl);
+        explicit Screen(SDL2::SDL& sdl, int width, int height);
     };
 };

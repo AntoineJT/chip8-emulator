70576435778c800b607e6f51e65f04c2bdc51d79
Antoine James Tournepiche <antoinejt.serveur@gmail.com>
2020-12-22 19:54:47 +0100

    Nuke the only new and delete in the code
    Thanks to Lynix for telling me how to do to avoid new and delete in this case

--- chip8dump/CurrentDateFunc.cpp
+++ chip8dump/CurrentDateFunc.cpp
@@ -6,13 +6,11 @@
 std::string CurrentDate() noexcept
 {
     constexpr std::size_t buffer_size = 50;
-    auto* buffer = new char[buffer_size];
+    std::string date(buffer_size, ' ');
 
     const std::time_t time = std::time(nullptr);
-    std::strftime(buffer, buffer_size, "%Y_%m_%d-%H_%M_%S", std::localtime(&time));
-
-    std::string date(buffer);
-    delete[] buffer;
+    const std::size_t size = std::strftime(date.data(), buffer_size, "%Y_%m_%d-%H_%M_%S", std::localtime(&time));
+    date.resize(size);
 
     return date;
 }

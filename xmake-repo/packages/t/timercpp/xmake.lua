22d5488e3c757482eb7726275ca138a927dc5050
Antoine James Tournepiche <antoinejt.serveur@gmail.com>
2021-09-09 03:32:31 +0200

    Still broken

--- xmake-repo/packages/t/timercpp/xmake.lua
+++ xmake-repo/packages/t/timercpp/xmake.lua
@@ -4,11 +4,10 @@ package("timercpp")
     set_description("Javascript like setTimeout and setInterval for c++ developers")
     set_license("MIT")
 
-    set_urls("https://github.com/99x/timercpp.git")
-    add_versions("15_04_2021", "fbf911046b46f4fa68e3a94d004acb3d9de41f10")
-
     -- this header only lib is made using C++14
-    add_patches("15_04_2021", path.join(os.scriptdir(), "patches", "15_04_2021", "cpp17.patch"), "0a0dab5abc31c6e3000ea4523ee5d377c4641a9c1d3b9201f4005254ec1b9d3d")
+    set_urls("https://github.com/99x/timercpp.git", "https://github.com/AntoineJT/timercpp-cpp17.git")
+    add_versions("15_04_2021", "fbf911046b46f4fa68e3a94d004acb3d9de41f10")
+    add_versions("cpp17", "cfb799d59b187bc82c2e3437dd20d40061c8390c")
 
     on_install(function (package)
         os.cp("timercpp.h", package:installdir("include"))

cddf19c0673aad9914c19f407b9679433daa2277
Antoine James Tournepiche <antoinejt.serveur@gmail.com>
2021-09-17 17:36:16 +0200

    Chip8Dump: Add --no-file parameter
    Allows to avoid dumping a lot of things to a file on disk

--- Chip8Dump/MemDumpApp.cpp
+++ Chip8Dump/MemDumpApp.cpp
@@ -2,6 +2,7 @@
 #include <cassert>
 #include <fstream>
 #include <iostream>
+#include <optional>
 
 #include "CurrentDateFunc.hpp"
 #include "DumpMemoryFunc.hpp"
@@ -15,15 +16,27 @@
 #include <SDL.h>
 #include <tclap/CmdLine.h>
 
+std::string ExecNextInstruction(Chip8::Memory& memory, Chip8::Machine& machine)
+{
+    const std::uint16_t opcode = memory.NextInstruction();
+    machine.HandleEvents();
+    machine.Execute(opcode);
+
+    // return disassembled instruction
+    return Chip8::Disasm::Opcode2Asm(opcode);
+}
+
 int main(int argc, char* argv[])
 {
     TCLAP::CmdLine cmd("Chip8Emu Dump Tool");
     TCLAP::UnlabeledValueArg<std::string> filenameArg("inputfile", "Chip8 game file.", true, "", "string");
     TCLAP::ValueArg<int> delayArg("d", "delay", "Time in milliseconds between instructions. Default: 500.", false, 500, "time in ms");
     TCLAP::SwitchArg noHeapSwitch("n", "no-heap", "Don't dump heap memory.", false);
+    TCLAP::SwitchArg noFileDumpSwitch("f", "no-file", "Only output basic infos on stdout.", false);
     cmd.add(filenameArg);
     cmd.add(delayArg);
     cmd.add(noHeapSwitch);
+    cmd.add(noFileDumpSwitch);
     cmd.parse(argc, argv);
 
     const std::string filename = filenameArg.getValue();
@@ -33,13 +46,17 @@ int main(int argc, char* argv[])
         delay = 0;
     }
     const bool noHeap = noHeapSwitch.getValue();
+    const bool noFile = noFileDumpSwitch.getValue();
 
     const std::string ofname = filename + "_" + CurrentDate() + "_dump.txt";
-    std::ofstream output(ofname);
-    if (!output.is_open())
-    {
-        std::cerr << "Can't create file '" << ofname << "'!" << std::endl;
-        return EXIT_FAILURE;
+    std::optional<std::ofstream> output = std::nullopt;
+    if (!noFile) {
+        output = std::ofstream(ofname);
+        if (!output->is_open())
+        {
+            std::cerr << "Can't create file '" << ofname << "'!" << std::endl;
+            return EXIT_FAILURE;
+        }
     }
 
     // sets up the emulator to be able to dump memory
@@ -64,18 +81,23 @@ int main(int argc, char* argv[])
     std::cout << "Press CTRL^C to quit" << std::endl;
 
     const auto& DumpMemoryFunc = noHeap ? Chip8::Dump::DumpMemoryWithoutHeap : Chip8::Dump::DumpMemory;
-    while(true)
-    {
-        const std::uint16_t opcode = memory.NextInstruction();
-        const std::string instruction = Chip8::Disasm::Opcode2Asm(opcode);
-
-        machine.HandleEvents();
-        machine.Execute(opcode);
-        std::cout << instruction << std::endl;
-        output << instruction << '\n'
-            << DumpMemoryFunc(memory) << '\n';
-
-        SDL_Delay(delay);
+    if (noFile) {
+        while (true)
+        {
+            std::cout << ExecNextInstruction(memory, machine) << std::endl;
+            SDL_Delay(delay);
+        }
+    }
+    else {
+        assert(output.has_value());
+        while (true)
+        {
+            const std::string instruction = ExecNextInstruction(memory, machine);
+            std::cout << instruction << std::endl;
+            *output << instruction << '\n'
+                << DumpMemoryFunc(memory) << '\n';
+            SDL_Delay(delay);
+        }
     }
 }
     

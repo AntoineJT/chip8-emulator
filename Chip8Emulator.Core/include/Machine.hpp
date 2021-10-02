be1b0ae41db0b82130a9628b8f99a96d3129b07e
TNtube <51389578+TNtube@users.noreply.github.com>
2021-10-02 20:39:58 +0200

    adding space between noreturn and function type

--- Chip8Emulator.Core/include/Machine.hpp
+++ Chip8Emulator.Core/include/Machine.hpp
@@ -30,6 +30,6 @@ namespace Chip8
 
         void ExecuteNextInstruction();
         void Execute(std::uint16_t opcode);
-        [[noreturn]]void HandleEvents();
+        [[noreturn]] void HandleEvents();
     };
 }
